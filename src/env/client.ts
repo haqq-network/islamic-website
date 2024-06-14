a1798ccf5105a28021e87933f7556147c9d0c3e9
Oleg Shilov <me@olegshilov.com>
2024-06-14 17:02:45 +0700

    feat: use environment variables for posthog credentials

--- src/env/client.ts
+++ src/env/client.ts
@@ -5,9 +5,13 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_GIT_COMMIT_SHA: z.string().min(3),
     NEXT_PUBLIC_TURNSTILE_SITEKEY: z.string().min(3),
+    NEXT_PUBLIC_POSTHOG_HOST: z.string().min(3).optional(),
+    NEXT_PUBLIC_POSTHOG_KEY: z.string().min(3).optional(),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_TURNSTILE_SITEKEY: process.env['NEXT_PUBLIC_TURNSTILE_SITEKEY'],
+    NEXT_PUBLIC_POSTHOG_HOST: process.env['NEXT_PUBLIC_POSTHOG_HOST'],
+    NEXT_PUBLIC_POSTHOG_KEY: process.env['NEXT_PUBLIC_POSTHOG_KEY'],
     NEXT_PUBLIC_GIT_COMMIT_SHA:
       process.env['GIT_COMMIT_SHA'] ??
       process.env['NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA'] ??
