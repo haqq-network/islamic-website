1735ddc34ad9fcf79fb7affc0f2411bc190a32e2
Oleg Shilov <me@olegshilov.com>
2024-06-15 18:47:38 +0700

    chore: update deps, run tests with swc compiler, use pnpm as package manager

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@
 const { composePlugins, withNx } = require('@nx/next');
 const { withSentryConfig } = require('@sentry/nextjs');
 const withNextIntl = require('next-intl/plugin');
-const jiti = require('jiti')(__filename);
 
 // Validate during build.
-jiti('./src/env/client.ts');
+// const jiti = require('jiti')(__filename);
+// jiti('./src/env/client.ts');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
